f1c4201376606c921a212822e4eefc0fc1175e61
Leigh-ann <leighann.gant@gmail.com>
2018-02-02 14:20:22 +0000

    Weather temp added

--- jasmine-standalone-2.9.1/src/thermostatJQuery.js
+++ jasmine-standalone-2.9.1/src/thermostatJQuery.js
@@ -1,3 +1,8 @@
+// var env = require('require-env');
+// api_key = env.require('API_KEY')
+//
+// console.log(api_key)
+
 $(document).ready(function(){
   var thermostat = new Thermostat;
   updateTemperature();
@@ -10,6 +15,11 @@ $(document).ready(function(){
     function hidePowerSaveButton(){
       if (thermostat.powerSaving) $("#turnPowerSaveOn").hide();
     }
+
+    $.get( `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=dfab0b20270092f2eb7795e16c8d3077&units=metric`, function( response ) {
+        $("#temp").html( response.main.temp );
+        $("#weather").html( response.weather[0].description );
+    });
 
   $("#turnPowerSaveOn").click(function(event){
     thermostat.powerSavingOn();
